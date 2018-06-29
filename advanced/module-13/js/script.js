bf9ceffb340bec6172b41d712e219a66883a82cb
Remas <remas.omar.101@gmail.com>
2018-06-29 22:36:06 +0300

    add HW module-13 n1

--- advanced/module-13/js/script.js
+++ advanced/module-13/js/script.js
@@ -5,9 +5,9 @@ const tel = document.getElementById("tel");
 const submitBtn = document.getElementById("submit-btn");
 const resultsList = document.querySelector(".results");
 
-const firstNameReg = /^([a-z]|[а-яё]|[а-яґєії`´ʼ’ʼ’])+(\s[a-z]+|\s[а-яё]+|\s[а-яґєії`´ʼ’ʼ’]+){0,2}$/ig;
-const lastNameReg = /^([a-z]|[а-яё]|[а-яґєії`´ʼ’ʼ’])+(([\s-]|(\s-\s))[a-z]+|([\s-]|(\s-\s))[а-яё]+|([\s-]|(\s-\s))[а-яґєії`´ʼ’ʼ’]+){0,1}$/ig;
-const telReg = /^\+\d{1}(-?\s?\d){11}$/g;
+const firstNameReg = /^([a-z]|[а-яё]|[а-яґєії`´ʼ’ʼ’])+(\s[a-z]+|\s[а-яё]+|\s[а-яґєії`´ʼ’ʼ’]+){0,2}$/i;
+const lastNameReg = /^([a-z]|[а-яё]|[а-яґєії`´ʼ’ʼ’])+(([\s-]|(\s-\s))[a-z]+|([\s-]|(\s-\s))[а-яё]+|([\s-]|(\s-\s))[а-яґєії`´ʼ’ʼ’]+){0,1}$/i;
+const telReg = /^\+\d{1}(-?\s?\d){11}$/;
 
 submitBtn.addEventListener("click", validate);
 
@@ -15,9 +15,9 @@ function validate(event) {
   event.preventDefault();
 
   const validateResults = {
-    firstNameResult: firstName.value.match(firstNameReg),
-    lastNameResult: lastName.value.match(lastNameReg),
-    telResult: tel.value.match(telReg)
+    firstNameResult: firstNameReg.test(firstName.value),
+    lastNameResult: lastNameReg.test(lastName.value),
+    telResult: telReg.test(tel.value)
   };
 
   showResults(validateResults);
